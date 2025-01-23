80e2abf7ab31b5df681f45fc05da697a28887c6e
champion87 <lidor071205@gmail.com>
2025-01-23 15:10:13 +0200

    This is the command that runs DANA's code. YAEL please make sure to use this one.

---
+++ dana.sh
@@ -0,0 +1,8 @@
+python DANA_inversion_save_latents.py \
+    --data_path /home/ML_courses/03683533_2024/lidor_yael_snir/lidor_only/cross-image-texturing/lidor/face_view_4.jpg \
+    --save_dir latents \
+    --sd_version 2.1 \
+    --seed 42 \
+    --steps 50 \
+    --inversion_prompt "a photo of clouds" \
+    # --extract-reverse
