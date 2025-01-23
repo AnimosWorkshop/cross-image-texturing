959f61c8458e365b23f3a7e8feeb3a08773561b2
champion87 <lidor071205@gmail.com>
2025-01-23 14:23:27 +0200

    "demo.py" demonstrates a fully working single image reconstruction over custom DM conditioning and initial latents

--- demo.py
+++ demo.py
@@ -1,39 +1,26 @@
-# !pip install opencv-python transformers accelerate
-from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
-from diffusers.utils import load_image
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler, DDIMScheduler
+from PIL import Image
 import numpy as np
 import torch
 
-import cv2
-from PIL import Image
 
-# download an image
-image = load_image(
-    "https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png"
+cond = Image.open("/home/ML_courses/03683533_2024/lidor_yael_snir/lidor_only/cross-image-texturing/lidor/face_cond_4.jpg")
+
+controlnet = ControlNetModel.from_pretrained(
+    "lllyasviel/sd-controlnet-depth", torch_dtype=torch.float16
 )
-image = np.array(image)
-
-# get canny image
-image = cv2.Canny(image, 100, 200)
-image = image[:, :, None]
-image = np.concatenate([image, image, image], axis=2)
-canny_image = Image.fromarray(image)
-
-# load control net and stable diffusion v1-5
-controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
 pipe = StableDiffusionControlNetPipeline.from_pretrained(
-    "runwayml/stable-diffusion-v1-5", controlnet=controlnet, torch_dtype=torch.float16
+    "runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
 )
+pipe.scheduler == DDIMScheduler.from_config(pipe.scheduler.config)
 
-# speed up diffusion process with faster scheduler and memory optimization
-pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
-# remove following line if xformers is not installed
 pipe.enable_xformers_memory_efficient_attention()
-
 pipe.enable_model_cpu_offload()
 
-# generate image
-generator = torch.manual_seed(0)
-image = pipe(
-    "futuristic-looking woman", num_inference_steps=20, generator=generator, image=canny_image
-).images[0].save("demo.png")
+latent = torch.load("/home/ML_courses/03683533_2024/lidor_yael_snir/lidor_only/cross-image-texturing/inverted/noisy/inverted_4.pt").to(torch.float16).unsqueeze(0)
+
+res = pipe("Portrait photo of Kratos, god of war.", cond, num_inference_steps=20, latents=latent).images[0]
+
+res.save('./holy_shit_that_works.png')
+
+
