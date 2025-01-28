a2a0b1dade4857b33109f7b94aff539d75f1fda4
champion87 <lidor071205@gmail.com>
2025-01-28 11:12:25 +0200

    dont need these

--- export.sh
+++
@@ -1,5 +0,0 @@
-# Copy these
-
-export TRANSFORMERS_CACHE=/home/ML_courses/03683533_2024/lidor_yael_snir/transformers_cache
-export HF_HOME=/home/ML_courses/03683533_2024/lidor_yael_snir/hf_home
-export NLTK_DATA=/home/ML_courses/03683533_2024/lidor_yael_snir/nltk_data
