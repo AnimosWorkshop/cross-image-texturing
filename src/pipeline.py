96c5fbd856b5a8a28f5a031c4e1fd5a0b9d3fde2
champion87 <lidor071205@gmail.com>
2025-03-20 11:29:49 +0200

    WIP

--- src/pipeline.py
+++ src/pipeline.py
@@ -37,7 +37,7 @@ from diffusers.training_utils import set_seed
 
 from transformers import CLIPImageProcessor, CLIPTextModel, CLIPTokenizer
 from project import UVProjection as UVP
-from project import default_cameras
+from project import default_cameras, build_uvp
 
 
 from SyncMVD.src.syncmvd.attention import SamplewiseAttnProcessor2_0, replace_attention_processors
@@ -216,34 +216,6 @@ class StableSyncMVDPipeline(StableDiffusionControlNetPipeline):
 		# 1. Initialize camera positions
 		# Define the cameras for rendering
 		self.centers = camera_centers
-  
-		# self.camera_poses = []
-		# self.attention_mask=[]
-
-		# cam_count = len(camera_azims)
-		# front_view_diff = 360
-		# back_view_diff = 360
-		# front_view_idx = 0
-		# back_view_idx = 0
-		# for i, azim in enumerate(camera_azims):
-		# 	if azim < 0:
-		# 		azim += 360
-		# 	self.camera_poses.append((0, azim))
-		# 	self.attention_mask.append([(cam_count+i-1)%cam_count, i, (i+1)%cam_count])
-		# 	if abs(azim) < front_view_diff:
-		# 		front_view_idx = i
-		# 		front_view_diff = abs(azim)
-		# 	if abs(azim - 180) < back_view_diff:
-		# 		back_view_idx = i
-		# 		back_view_diff = abs(azim - 180)
-
-		# # Add two additional cameras for painting the top surfaces
-		# if top_cameras:
-		# 	self.camera_poses.append((30, 0))
-		# 	self.camera_poses.append((30, 180))
-
-		# 	self.attention_mask.append([front_view_idx, cam_count])
-		# 	self.attention_mask.append([back_view_idx, cam_count+1])
 		self.camera_poses, self.attention_mask = default_cameras()
   
 
@@ -251,17 +223,21 @@ class StableSyncMVDPipeline(StableDiffusionControlNetPipeline):
 		# Set up pytorch3D for projection between screen space and UV space
 		# uvp is for latent and uvp_rgb for rgb color
 		#LIDOR TODO
-		self.uvp = UVP(texture_size=texture_size, render_size=latent_size, sampling_mode="nearest", channels=4, device=self._execution_device)
-		if mesh_path.lower().endswith(".obj"):
-			self.uvp.load_mesh(mesh_path, scale_factor=mesh_transform["scale"] or 1, autouv=mesh_autouv)
-		elif mesh_path.lower().endswith(".glb"):
-			# mesh_autouv=False
-   #TODO change to load_glb_mesh, or directly to build_uvp
-			self.uvp.load_mesh(mesh_path, scale_factor=mesh_transform["scale"] or 1, autouv=mesh_autouv)
-		else:
-			assert False, "The mesh file format is not supported. Use .obj or .glb."
+# 		self.uvp = UVP(texture_size=texture_size, render_size=latent_size, sampling_mode="nearest", channels=4, device=self._execution_device)
+# 		if mesh_path.lower().endswith(".obj"):
+# 			self.uvp.load_mesh(mesh_path, scale_factor=mesh_transform["scale"] or 1, autouv=mesh_autouv)
+# 		elif mesh_path.lower().endswith(".glb"):
+# 			# mesh_autouv=False
+#    #TODO change to load_glb_mesh, or directly to build_uvp
+# 			self.uvp.load_mesh(mesh_path, scale_factor=mesh_transform["scale"] or 1, autouv=mesh_autouv)
+# 		else:
+# 			assert False, "The mesh file format is not supported. Use .obj or .glb."
 
-		self.uvp.set_cameras_and_render_settings(self.camera_poses, centers=camera_centers, camera_distance=4.0)
+# 		self.uvp.set_cameras_and_render_settings(self.camera_poses, centers=camera_centers, camera_distance=4.0)
+
+		self.uvp = build_uvp(mesh_path, texture_size=texture_size, render_size=latent_size, sampling_mode="nearest", channels=4, device=self._execution_device)
+
+
 
 		self.uvp_rgb = UVP(texture_size=texture_rgb_size, render_size=render_rgb_size, sampling_mode="nearest", channels=3, device=self._execution_device)
 		self.uvp_rgb.mesh = self.uvp.mesh.clone()
